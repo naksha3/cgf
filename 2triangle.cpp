bf8486d1cc992ffaea048748aa2d3039f38ab9cd
naksha3 <114646116+naksha3@users.noreply.github.com>
2023-02-27 11:54:51 +0530

    Update 2triangle.cpp

--- 2triangle.cpp
+++ 2triangle.cpp
@@ -1,1 +1,45 @@
+#include<GL/glut.h>
+#include<windows.h>
+#include<GL/glu.h>
+#include<stdio.h>
+GLfloat R,px,py;
+void Draw(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0,0,1);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(0.1,0.4);
+        glVertex2f(0.7,0.4);
+        glVertex2f(0.4,0.8);
+    glEnd();
+    glLoadIdentity();
+    glTranslatef(px,py,0);
+    glRotatef(R,0,0,1);
+    glTranslatef(-px,-py,0);
+    glColor3f(1,0,0);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(0.1,0.4);
+        glVertex2f(0.7,0.4);
+        glVertex2f(0.4,0.8);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc,char ** argv){
+
+
+printf("Enter the reference point");
+scanf("%f%f",&px,&py);
+printf("Enter the rotation");
+scanf("%f",&R);
+glutInit(&argc,argv);
+
+glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
+glutInitWindowPosition(150,250);
+glutInitWindowSize(500,500);
+
+glutCreateWindow("Triangle");
+glutDisplayFunc(Draw);
+glutMainLoop();
+return 0;
+}
 
